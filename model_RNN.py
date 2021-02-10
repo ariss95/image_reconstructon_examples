98234528d001f08f95b9b5753fb6a22f83987750
Aris Siozos <arisiozos@yahoo.gr>
2021-02-10 19:51:59 +0200

    Add files via upload

---
+++ model_RNN.py
@@ -0,0 +1,20 @@
+import data_loader
+import torch
+import torch.nn as nn
+
+class first_RNN(nn.Module):
+    def __init__(self, input):
+        super(first_RNN, self).__init__()
+        #TODO add layers
+        self.device = torch.device('cpu')
+        self.original_input = input #add size
+        self.compressed_input = int(self.original_input/8) #from 64*64 to 32*32
+        self.compression = nn.Linear(self.original_input, self.compressed_input).to(device=self.device)
+        self.rnn = nn.RNN(input_size=self.compressed_input, hidden_size=self.original_input, num_layers=1).to(device=self.device)
+        self.output = nn.Linear(self.original_input, self.original_input, bias=False).to(device=self.device)
+
+    def forward(self, data):
+        compressed = self.compression(data)
+        out, hidden = self.rnn(compressed, None)# NONE for h0 = [0..0]
+        out = self.output(out)
+        return out
---
+++ train_RNN.py
@@ -0,0 +1,92 @@
+import data_loader as dl
+import model_RNN
+import torch
+import torch.nn as nn
+import torchvision
+import matplotlib.pyplot as plt
+
+path = 'movingMnist/mnist_test_seq.npy'
+data_loader = dl.Moving_MNIST_Loader(path=path, time_steps=20, load_only=4000,
+                                      flatten=True, scale=False)
+device = torch.device('cpu')
+model = model_RNN.first_RNN(4096).to(device)
+learning_rate = 0.1
+epochs = 7
+batch_size = 64
+training_samples = 3200
+#print(data_loader.data[0].shape)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+
+
+def fit(model, dataloader):
+	model.train()
+	loss = 0.0
+	loss_epoch=[]
+	iterations = int(training_samples/batch_size)
+	for epoch in range(epochs):
+		loss = 0.0
+		for j in range (iterations):
+			#print(j)
+			data = dataloader.load_batch_train(batch_size)
+			input = torch.tensor(data, dtype=torch.float32)
+			output = model.forward(input)
+			loss_func = torch.mean((output - input) ** 2)
+			loss += loss_func.item()
+			optimizer.zero_grad()
+			loss_func.backward()
+			optimizer.step()
+			if j == (iterations-1):
+				x1 = output[0][0].view(1,64,64)
+				x1 = x1.permute(1, 2, 0)
+				x1 = x1.detach().numpy()
+				plt.figure(epoch)
+				plt.clf()
+				plt.title('our method')
+				plt.imshow(x1, cmap="gray")
+				plt.pause(1)
+				plt.draw()
+		loss_epoch.append(loss)
+	return loss_epoch
+
+training_loss = fit(model,data_loader)
+print(training_loss)
+
+
+
+
+
+
+
+
+'''
+batch = data_loader.load_batch_train(50)
+input = torch.tensor(batch, dtype=torch.float32)
+
+x = model.compression(input)
+x1 = x[0][0].view(1,32,32)
+x1 = x1.permute(1, 2, 0)
+x1 = x1.detach().numpy()
+x1[x1 < 0] = 0
+x1[x1 > 255] = 255
+print(x1.shape)
+plt.figure(1)
+plt.clf()
+plt.title('our method')
+plt.imshow(x1, cmap="gray")
+plt.show()
+print(x1)
+#output = model.forward(input)
+#'#''
+for j in range(3):
+    for i in range(20):
+        #first of every 20 is the same video
+        clip = batch[i][j]
+        #clip = 255 - clip
+        plt.figure(1)
+        plt.clf()
+        plt.title('our method')
+        plt.imshow(clip, cmap='gray')
+        plt.pause(1)
+        plt.draw()  
+'''
